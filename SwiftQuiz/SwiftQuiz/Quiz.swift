48151aa20d36ff87944093d5d333989b5026a445
felippedamiao <34101824+felippedamiao@users.noreply.github.com>
2018-05-16 11:30:25 -0300

    Add files via upload

--- SwiftQuiz/SwiftQuiz/Quiz.swift
+++ SwiftQuiz/SwiftQuiz/Quiz.swift
@@ -10,4 +10,22 @@ import Foundation
 
 class Quiz{
     
+    let question: String
+    let options: [String]
+    private let correctAnswer: String
+    
+    init(question: String, options: [String], correctAnswer: String) {
+        self.question = question
+        self.options = options
+        self.correctAnswer = correctAnswer
+    }
+    
+    func validateOption(_ index: Int) -> Bool {
+        let answer  = options[index]
+        return answer == correctAnswer
+    }
+    
+    deinit {
+        print("Liberou quiz de memória")
+    }
 }
